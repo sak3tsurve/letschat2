3219b9559b2c1f059d0671a52fdebe87a450139b
saket54 <89304756+sak3tsurve@users.noreply.github.com>
2023-05-14 17:50:39 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,22 @@
+var firebaseConfig = {
+
+    apiKey: "AIzaSyD99DsDR58Z5N8dR2lLxvRxyq3wNIznroY",
+  
+    authDomain: "kwitter-7f23b.firebaseapp.com",
+  
+    databaseURL: "https://kwitter-7f23b-default-rtdb.firebaseio.com",
+  
+    projectId: "kwitter-7f23b",
+  
+    storageBucket: "kwitter-7f23b.appspot.com",
+  
+    messagingSenderId: "474378443490",
+  
+    appId: "1:474378443490:web:af06ad867c5807ff2d1cbb"
+  
+  };
+  
+  
+  // Initialize Firebase
+  
+  firebase.initializeApp(firebaseConfig);
